3178375a3a06f199eafe7bd14e1be751d8bbf93b
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-16 01:53:28 +0700

    Fix webpack cache and resolution errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     loader: 'custom',
     loaderFile: './my-loader.ts',
   },
 }
-
 
 export default nextConfig;
