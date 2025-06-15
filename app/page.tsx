831a0585c6c98b9023a25822150c684e264a3d2b
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-15 22:25:36 +0700

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import Banner from "./components/banner";
+import How from "./components/how";
+import Lean from "./components/lean";
+import FAQ from "./components/FAQ";
+import Column from "./components/column";
+
 export default function Home() {
   return (
-    <div>
-      {/* Your JavaScript code will go here */}
-    </div>
+    <>
+      <Column className="min-h-screen gap-24">
+        <Banner />
+        <How />
+        <Lean />
+        <FAQ />
+      </Column>
+    </>
   );
 }
