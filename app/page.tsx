0d99987e982cefe18b99dade096a7f8be98e2f4a
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-15 22:24:27 +0700

    Remove UI components from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import Banner from "./components/banner";
-import How from "./components/how";
-import Lean from "./components/lean";
-import FAQ from "./components/FAQ";
-import Column from "./components/column";
-
 export default function Home() {
   return (
-    <>
-      <Column className="min-h-screen gap-24">
-        <Banner />
-        <How />
-        <Lean />
-        <FAQ />
-      </Column>
-    </>
+    <div>
+      {/* Your JavaScript code will go here */}
+    </div>
   );
 }
