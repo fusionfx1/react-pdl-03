88af1e02c0ff605ae49258b10d051030895868d4
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-15 22:30:38 +0700

    Update loan table with new amounts

--- app/components/table-lean.tsx
+++ app/components/table-lean.tsx
@@ -5,6 +5,17 @@ type Props = {};
 const TableLean = (props: Props) => {
   const tableData = [
     {
+      amount: "$500",
+      rate: "28.00%",
+      term: "6 months",
+      fee: "2.00%",
+      feeCost: "$10.00",
+      repayment: "$95.33",
+      apr: "32.45%",
+      totalRepayments: "$571.98",
+      totalCosts: "$81.98"
+    },
+    {
       amount: "$1,000",
       rate: "24.00%",
       term: "12 months",
@@ -16,15 +27,26 @@ const TableLean = (props: Props) => {
       totalCosts: "$164.72"
     },
     {
-      amount: "$2,000",
+      amount: "$1,500",
+      rate: "21.00%",
+      term: "18 months",
+      fee: "4.00%",
+      feeCost: "$60.00",
+      repayment: "$96.78",
+      apr: "26.15%",
+      totalRepayments: "$1,742.04",
+      totalCosts: "$302.04"
+    },
+    {
+      amount: "$2,500",
       rate: "19.00%",
       term: "24 months",
       fee: "5.00%",
-      feeCost: "$100.00",
-      repayment: "$100.82",
+      feeCost: "$125.00",
+      repayment: "$126.03",
       apr: "24.12%",
-      totalRepayments: "$2,419.68",
-      totalCosts: "$519.68"
+      totalRepayments: "$3,024.72",
+      totalCosts: "$649.72"
     },
     {
       amount: "$5,000",
@@ -36,17 +58,6 @@ const TableLean = (props: Props) => {
       apr: "18.23%",
       totalRepayments: "$6,320.12",
       totalCosts: "$1,720.12"
-    },
-    {
-      amount: "$10,000",
-      rate: "7.90%",
-      term: "60 months",
-      fee: "10.00%",
-      feeCost: "$1000.00",
-      repayment: "$202.28",
-      apr: "9.20%",
-      totalRepayments: "$12,136.80",
-      totalCosts: "$3,136.80"
     }
   ];
 
