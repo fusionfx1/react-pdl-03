c28a5292476422c978350a834ea8841c376390df
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-15 23:41:14 +0700

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        main: "#3377ff",
-        "green-dark": "#3377ff",
+        main: "#0055ff",
+        "green-dark": "#0055ff",
       },
     },
   },
