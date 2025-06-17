80cfb764a93b961fd91234192174d342d0c58b91
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-18 06:13:00 +0700

    Updated my-loader.ts

--- my-loader.ts
+++ my-loader.ts
@@ -8,6 +8,6 @@ export default function cloudinaryLoader({
   quality?: number;
 }) {
   const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
-  const serverUrl = "https://scratchpayforpets.com";
+  const serverUrl = "https://react-pdl-01.vercel.app";
   return `${serverUrl}/${src}`;
 }
