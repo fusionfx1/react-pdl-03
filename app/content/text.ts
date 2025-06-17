eec92174b28546b8739356e7aa0d98fe591f80e7
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-06-17 18:44:07 +0700

    Updated text.ts

--- app/content/text.ts
+++ app/content/text.ts
@@ -3,7 +3,7 @@ export const content = {
   address: "591 Laurel Common Matfield Green, KS 66862",
   company: "74512",
   colors: "#245bdf",
-  title1: "Let's Get to The Point.",
+  title1: "Simple, Friendly Payment Plans",
   title2: "Medical Financing with Heart.",
   aid: "14881",
   gtm: "GTM-XXXXXXX", // แทนที่ด้วย GTM ID จริงของคุณ
